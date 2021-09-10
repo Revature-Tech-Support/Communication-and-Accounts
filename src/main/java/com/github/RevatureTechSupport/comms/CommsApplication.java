af1212a45d3ef4f8c85ae65756a4fb29bbb219a9
tam <87770920+tamhpn@users.noreply.github.com>
2021-09-09 23:48:39 -0500

    initial implementation of a reactive websocket chat application

---
+++ src/main/java/com/github/RevatureTechSupport/comms/CommsApplication.java
@@ -0,0 +1,12 @@
+package com.github.RevatureTechSupport.comms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommsApplication.class, args);
+	}
+}

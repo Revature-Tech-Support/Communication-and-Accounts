b1df63644a1ccf25d9b30be594a77eccbe6ebf06
tam <87770920+tamhpn@users.noreply.github.com>
2021-09-18 00:50:12 -0500

    test...

--- src/test/java/com/github/RevatureTechSupport/comms/CommsApplicationTests.java
+++ src/test/java/com/github/RevatureTechSupport/comms/CommsApplicationTests.java
@@ -1,13 +1,30 @@
 package com.github.RevatureTechSupport.comms;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.net.URI;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.reactive.socket.WebSocketMessage;
+import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClient;
+import org.springframework.web.reactive.socket.client.WebSocketClient;
+
+import reactor.core.publisher.Mono;
 
 @SpringBootTest
 class CommsApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void sendingMessageReturnsMessage() {
+		WebSocketClient client = new ReactorNettyWebSocketClient();
+		URI uri = URI.create("ws://localhost:8080/ws");	
+		
+		client.execute(uri,
+			session -> session.send(Mono.just(session.textMessage("test")))
+				.thenMany(session.receive())
+				.map(WebSocketMessage::getPayloadAsText)
+				.then())
+			.block();
 	}
-
 }

dfe9fc70fe1dbb72c8d4b3a77f1185ae17b71eea
tam <87770920+tamhpn@users.noreply.github.com>
2021-09-19 16:54:32 -0500

    removed message type

--- src/main/java/com/github/RevatureTechSupport/comms/WSHandler.java
+++ src/main/java/com/github/RevatureTechSupport/comms/WSHandler.java
@@ -1,8 +1,5 @@
 package com.github.RevatureTechSupport.comms;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.socket.WebSocketHandler;
 import org.springframework.web.reactive.socket.WebSocketMessage;
@@ -17,23 +14,20 @@ public class WSHandler implements WebSocketHandler {
     // https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Sinks.html
     // many() enables multicasting (broadcasting to multiple subscribers)
     // replay() retains (all) history
-    private Sinks.Many<Message> sink = Sinks.many().replay().all();
-    private Flux<Message> messages = sink.asFlux();
-
-    public static ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private Sinks.Many<String> sink = Sinks.many().replay().all();
+    private Flux<String> messages = sink.asFlux();
 
     @Override
     public Mono<Void> handle(WebSocketSession webSocketSession) {
         webSocketSession.receive()
             .map(WebSocketMessage::getPayloadAsText)
-            .map(this::deserializeMessage)
             .log()
             .subscribe(this::onNext, this::onError, this::onComplete);
 
-        return webSocketSession.send(messages.map(this::serializeMessage).map(webSocketSession::textMessage));
+        return webSocketSession.send(messages.map(webSocketSession::textMessage));
     }
 
-    private void onNext(Message message) {
+    private void onNext(String message) {
         sink.tryEmitNext(message);
     }
 
@@ -47,27 +41,5 @@ public class WSHandler implements WebSocketHandler {
             sink = Sinks.many().replay().all();
             messages = sink.asFlux();
         }
-    }
-
-    private Message deserializeMessage(String string) {
-        Message message;
-        try {
-            message = OBJECT_MAPPER.readValue(string, Message.class);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-            message = null;
-        }
-        return message;
-    }
-
-    private String serializeMessage(Message message) {
-        String string;
-        try {
-            string = OBJECT_MAPPER.writeValueAsString(message);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-            string = null;
-        }
-        return string;
     }
 }
